be8e4c69111f1d44fcdf8d09802c177c68f51047
Stahl <Tom.Stahl@quintiles.com>
2018-01-22 11:14:02 -0500

    initial commit

---
+++ Controllers/FileController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace CoreFileUploadSvcProto.Controllers
+{
+    [Route("api/[controller]")]
+    public class FileController : Controller
+    {
+        // POST: /Account/Register
+        [HttpPost]
+        public async Task Upload(IFormFile file)
+        {
+            if (file == null) throw new Exception("File is null");
+            if (file.Length == 0) throw new Exception("File is empty");
+
+            using (Stream stream = file.OpenReadStream())
+            {
+                using (var binaryReader = new BinaryReader(stream))
+                {
+                    var fileContent = binaryReader.ReadBytes((int)file.Length);
+                    //await _uploadService.AddFile(fileContent, file.FileName, file.ContentType);
+                }
+            }
+        }
+    }
+}
